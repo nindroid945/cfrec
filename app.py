5cf8ee1b910580c97b77833f16f9593f0043deb3
JasonFeng365 <34261800+JasonFeng365@users.noreply.github.com>
2024-04-28 09:14:55 -0700

    Fellas, I cooked

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from Recommend import smart_recommend, pub_recommend
 from flask import Flask, request, jsonify, render_template
 import propelauth_flask as propel
-import time
+
 app = Flask(__name__)
 auth = propel.init_auth("https://69808087.propelauthtest.com", "b7574e339d28d2e617e203f9e417bdbdc8209fea143b558154dc505f40410fc38248e7de23eb0ca7e5bd337365eacba6")
 
 @app.route("/")
 def home():
 	return render_template("homepage.html")
+
+@app.route("/account/")
+def account():
+	return render_template("account.html")
 
 # def recommend(rating: int, tags: set, num: int):
 @app.route("/api/recommend/")
@@ -18,19 +22,34 @@ def recommend():
 	num = 5
 
 	response = jsonify(pub_recommend(rating, tags, num))
-	# response = jsonify(pub_recommend(1100, {"dp"}, 5))
-	# print(pub_recommend(1100, {"dp"}, 5))
 	response.headers.add('Access-Control-Allow-Origin', '*')
 	return response
 
 # def smart_recommend(handle: str) -> list:
 @app.route("/api/smartrecommend/")
 @auth.require_user
-def smartrecommend():	
+def smartrecommend():
 	# handle = database.getHandleFromPropelID(propel.current_user.user_id)
+	# if handle==None: return None
 	handle = "jasonfeng365"
 	# print(handle)
 
 	response = jsonify(smart_recommend(handle))
 	response.headers.add('Access-Control-Allow-Origin', '*')
 	return response
+
+@app.route("/api/linkaccount/", methods=['POST'])
+@auth.require_user
+def linkaccount():
+	username = request.headers['CFUsername']
+	# handle = database.setHandleFromPropelID(propel.current_user.user_id, username)
+	# if handle==None: return None
+	# print(handle)
+	return "Success"
+
+@app.route("/api/getlinkedaccount/")
+@auth.require_user
+def getlinkedaccount():
+	# handle = database.getHandleFromPropelID(propel.current_user.user_id)
+	# if handle: return handle
+	return "jasonfeng365"
