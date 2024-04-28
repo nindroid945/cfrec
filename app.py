08b690c6c20a19bbd9bc41057c7ad5bb72941601
JasonFeng365 <34261800+JasonFeng365@users.noreply.github.com>
2024-04-28 13:44:01 -0700

    Update backend with CF authentication

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import Random
 from flask import Flask, request, jsonify, render_template
 import propelauth_flask as propel
 import sqlite3
+import requests
+
 
 app = Flask(__name__)
 auth = propel.init_auth("https://69808087.propelauthtest.com", "b7574e339d28d2e617e203f9e417bdbdc8209fea143b558154dc505f40410fc38248e7de23eb0ca7e5bd337365eacba6")
@@ -56,13 +58,27 @@ def daily():
 @app.route("/api/linkaccount/", methods=['POST'])
 @auth.require_user
 def apilinkaccount():
+	handle = request.headers['CFUsername']
+	user_id = propel.current_user.user_id
+
+	response = requests.get(f"https://codeforces.com/api/user.status?handle={handle}&from=1&count=1")
+
+	if not response.ok:
+		return f"Unable to link: Response errored, Code {response.status_code}"
+	
+	rdict = response.json()['result']
+	if len(rdict) != 1 or 'verdict' not in rdict[0]:
+		return "Unable to link: It looks like you haven't solved any problems yet. Maybe Codeforces is still processing your submission?"
+	
+	if rdict[0]['problem']['contestId'] != 4 or rdict[0]['problem']['index'] != "A" or rdict[0]['verdict'] != "COMPILATION_ERROR":
+		verdict = rdict[0].get('verdict')
+	
+		print(f"Verdict Error, {verdict = }")
+		return "Unable to link: Please run `/link` again and ensure you submit a Compile Error to https://codeforces.com/problemset/problem/4/A and then click the checkmark."
+
 	con = sqlite3.connect("users.db")
 	cursor = con.cursor()
 	cursor.row_factory = sqlite3.Row
-
-
-	handle = request.headers['CFUsername']
-	user_id = propel.current_user.user_id
 
 	c = f"""
 SELECT * FROM cf_users
@@ -87,7 +103,7 @@ VALUES ('{handle}', '{user_id}')
 """
 		cursor.execute(s)
 		con.commit()
-	return "Success"
+	return "Successfully linked!"
 
 @app.route("/api/getlinkedaccount/")
 @auth.require_user
